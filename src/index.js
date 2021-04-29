1634cdf14ebbf04c9594d4838cce8d230d706b97
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-04-29 10:17:14 -0500

    Webpack Dev Server

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
 
-ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById('App'));
+ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById('app'));
