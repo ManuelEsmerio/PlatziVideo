ebdca7f43b8d6b52afc220b7afe5a432376d8f8b
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-04-29 11:17:22 -0500

    Component Header created

---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+import '../assets/styles/App.scss';
+
+const App = () => {
+    return (
+        <Header>
+            
+        </Header>
+    )
+}
+
+export default App
