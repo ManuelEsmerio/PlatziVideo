98815458f613e8d725329124f01896dbdc578630
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-04-29 11:24:32 -0500

    Component Search Created

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
+import Search from '../components/Search';
 
 import '../assets/styles/App.scss';
 
 const App = () => {
     return (
-        <Header>
-            
-        </Header>
+        <>
+            <Header />
+            <Search />
+        </>
     )
 }
 
