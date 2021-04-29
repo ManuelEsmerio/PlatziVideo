94a3566409f67953e0f02b4ab727a3eb9cb56fb5
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-04-29 13:12:25 -0500

    Course Complete [Curso Práctivo de React JS]

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,47 +1,56 @@
-import React from 'react';
+import React , { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import Item from '../components/Item';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
+
+
 const App = () => {
+
+    const API = 'http://localhost:3000/initialstate';
+
+    const { mylist, trends, originals } = useInitialState(API);
+
     return (
         <>
             <Header />
             <Search />
-            <Categories title="Mi lista!">
-                <Carousel>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                </Carousel>
-            </Categories>
 
-            <Categories title="Tendencias">
-                <Carousel>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                </Carousel>
-            </Categories>
+            { mylist.length > 0 &&
+                <Categories title="Mi lista!">
+                    <Carousel>
+                        {mylist.map(item =>
+                                <Item key={ item.id } {...item} />
+                        )}
+                    </Carousel>
+                </Categories>
+            }
 
-            <Categories title="Originales de PlatziVideo">
-                <Carousel>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                </Carousel>
-            </Categories>
+            { trends.length > 0 &&
+                <Categories title="Tendencias">
+                    <Carousel>
+                        {trends.map(item =>
+                                <Item key={ item.id } {...item} />
+                        )}
+                    </Carousel>
+                </Categories>
+            }
+
+            { originals.length > 0 &&
+                <Categories title="Originales de PlatziVideo">
+                    <Carousel>
+                        {originals.map(item =>
+                                <Item key={ item.id } {...item} />
+                        )}
+                    </Carousel>
+                </Categories>
+            }
 
             <Footer />
         </>
